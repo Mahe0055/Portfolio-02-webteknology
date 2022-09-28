b3ee4c9cc478d1fc9dac8993f38b683c05d6cfc6
Mahe0055 <mahe00555@stud.kea.dk>
2022-09-28 11:34:04 +0200

    making of "get result" button

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ console.log(button)
 button.addEventListener('click', function() {
     console.log('Get result')
 });
+
+//Borrowed code from class group in teams.
+console.log("script initiated");
+const buttonElement = document.querySelector('button')
+button.addEventListener('click', function() {
+    const input = document.querySelectorAll('input');
+    function gender (sel) {
+        alert(sel.options[sel.selectedIndex].text);
+    const age = input.value;
+    const bedtime = input.value;
+    const wakeUptime = input.value;
+    const lackOfSleep = wakeUptime - bedtime
+    inputTag.innerHTML = ;
+
+    }
+});
