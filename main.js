c6ba4cbda87c6ee8e6c33a701244678cf5567f57
Mahe0055 <mahe00555@stud.kea.dk>
2022-09-28 11:02:50 +0200

    making of "get result" button

---
+++ main.js
@@ -0,0 +1,6 @@
+//Borrowed code from class group in teams.
+const button = document.querySelector('button')
+console.log(button)
+button.addEventListener('click', function() {
+    console.log('Get result')
+});
